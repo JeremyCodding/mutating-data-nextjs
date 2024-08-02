083b4c84ebe17fa16dcbdd77088888ecc716a530
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-08-02 09:45:56 -0300

    Uploading images with cloudinary

--- actions/posts.js
+++ actions/posts.js
@@ -1,5 +1,6 @@
 "use server";
 
+import { uploadImage } from "@/lib/cloudinary";
 import { storePost } from "@/lib/posts";
 import { redirect } from "next/navigation";
 
@@ -24,8 +25,18 @@ export async function createPost(prevFormData, formData) {
     return { errors };
   }
 
+  let imageUrl;
+
+  try {
+    imageUrl = await uploadImage(image);
+  } catch (error) {
+    throw new Error(
+      "Image upload failed, post was not created. Try again later."
+    );
+  }
+
   await storePost({
-    imageUrl: "",
+    imageUrl,
     title,
     content,
     userId: 1,
